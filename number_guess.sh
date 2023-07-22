ef62301452b6e41a70a9b0d2db0d4ba0c5b14a5c
Pawel Walczewski <pwalczewski@gmail.com>
2023-07-22 12:41:11 -0400

    feat: add game stats to welcome message

--- number_guess.sh
+++ number_guess.sh
@@ -15,14 +15,19 @@ IFS="|"
 read USER_ID NAME <<< $USERDATA
 IFS=$ORIGINAL_IFS
 if [ $NAME ]; then
-  echo "Welcome back, $NAME"
+  GAMEDATA=$($PSQL "select count(*), min(guesses) from games where user_id = $USER_ID")
+  IFS="|"
+  read COUNT MIN <<< $GAMEDATA
+  IFS=$ORIGINAL_IFS
+  echo "Welcome back, $NAME! You have played $COUNT games, and your best game took $MIN guesses."
 else
   USER_ID=$($PSQL "insert into users(name) values('$USERNAME') returning user_id")
   if [ -z $USER_ID ]; then
     echo "Error: couldn't add a user. Exiting."
     exit 1
   fi
-  echo "Welcome, $USERNAME"
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
 fi
+echo Guess the secret number between 1 and 1000:
 }
 MAIN

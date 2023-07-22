971775650f5c5593a42417e9ee1937c111517666
Pawel Walczewski <pwalczewski@gmail.com>
2023-07-22 13:36:26 -0400

    fix: simple game loop

--- number_guess.sh
+++ number_guess.sh
@@ -36,15 +36,17 @@ echo random: $RANDOM_NUMBER
 echo Guess the secret number between 1 and 10:
 read GUESS
 TRIES=1
-while [ "$GUESS" != "$RANDOM_NUMBER" ]; do
+while [ $GUESS != $RANDOM_NUMBER ]; do
+  #echo loop $TRIES
+  if [[ $GUESS -lt $RANDOM_NUMBER ]]; then
+    HI_LO="higher"
+  else
+    HI_LO="lower"
+  fi
+  echo -e "It's $HI_LO than that, guess again: "
+
   read GUESS
   ((TRIES++))
-  if [ "$GUESS" -lt "$RANDOM_NUMBER" ]; then
-    $HI_LO="higer"
-  else
-    $HI_LO="lower"
-  fi
-  echo "It's $HI_LO than that, guess again: "
 done
 echo tries: $TRIES
 }
